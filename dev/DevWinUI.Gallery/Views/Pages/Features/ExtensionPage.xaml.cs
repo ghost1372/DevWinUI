1bf0e015ad4c8a21626c2d769b3038a4d9e15205
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-11-25 14:23:11 +0330

    Fix AOT/Trim Crash for Gallery App

--- dev/DevWinUI.Gallery/Views/Pages/Features/ExtensionPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Pages/Features/ExtensionPage.xaml.cs
@@ -1,7 +1,18 @@
-﻿namespace DevWinUIGallery.Views;
+﻿using System.Collections.ObjectModel;
+
+namespace DevWinUIGallery.Views;
 
 public sealed partial class ExtensionPage : Page
 {
+    public ObservableCollection<Animal> Items
+    {
+        get { return (ObservableCollection<Animal>)GetValue(ItemsProperty); }
+        set { SetValue(ItemsProperty, value); }
+    }
+
+    public static readonly DependencyProperty ItemsProperty =
+        DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<Animal>), typeof(ExtensionPage), new PropertyMetadata(new ObservableCollection<Animal>(Enum.GetValues<Animal>())));
+
     public ExtensionPage()
     {
         this.InitializeComponent();
--- dev/DevWinUI.Gallery/Views/Pages/Features/SwitchPresenterPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Pages/Features/SwitchPresenterPage.xaml.cs
@@ -1,6 +1,17 @@
-﻿namespace DevWinUIGallery.Views;
+﻿using System.Collections.ObjectModel;
+
+namespace DevWinUIGallery.Views;
 public sealed partial class SwitchPresenterPage : Page
 {
+    public ObservableCollection<Animal> Items
+    {
+        get { return (ObservableCollection<Animal>)GetValue(ItemsProperty); }
+        set { SetValue(ItemsProperty, value); }
+    }
+
+    public static readonly DependencyProperty ItemsProperty =
+        DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<Animal>), typeof(SwitchPresenterPage), new PropertyMetadata(new ObservableCollection<Animal>(Enum.GetValues<Animal>())));
+
     public SwitchPresenterPage()
     {
         this.InitializeComponent();
