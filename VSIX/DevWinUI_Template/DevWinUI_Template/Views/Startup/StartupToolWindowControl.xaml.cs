0672c652d308678f96f757ae5664ec260314f4a6
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-12-08 18:00:18 +0330

    Improve VSIX Startup Page

--- VSIX/DevWinUI_Template/DevWinUI_Template/Views/Startup/StartupToolWindowControl.xaml.cs
+++ VSIX/DevWinUI_Template/DevWinUI_Template/Views/Startup/StartupToolWindowControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace DevWinUI_Template.Views.Startup;
 
+using System.Diagnostics;
 using System.Windows.Controls;
 
 public partial class StartupToolWindowControl : UserControl
@@ -7,5 +8,10 @@ public partial class StartupToolWindowControl : UserControl
     public StartupToolWindowControl()
     {
         this.InitializeComponent();
+    }
+
+    private void ImageIcon_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        Process.Start(MSStoreHyperLink.NavigateUri);
     }
 }
