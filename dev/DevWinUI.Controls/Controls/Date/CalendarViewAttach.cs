0a467ce2b16b8eafb235840a36da4df4adfcb8c7
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-12-22 14:33:14 +0330

    Fix CalendarViewStyle and Clock Border Background

--- dev/DevWinUI.Controls/Controls/Date/CalendarViewAttach.cs
+++ dev/DevWinUI.Controls/Controls/Date/CalendarViewAttach.cs
@@ -20,18 +20,24 @@ internal partial class CalendarViewAttach
         {
             FrameworkElement headerBorder = DependencyObjectEx.FindDescendant(calendarView, "PART_HeaderBorder");
             FrameworkElement topBorder = DependencyObjectEx.FindDescendant(calendarView, "PART_TopBorder");
+            var headerButton = DependencyObjectEx.FindDescendant(calendarView, "HeaderButton") as Button;
+            var nextButton = DependencyObjectEx.FindDescendant(calendarView, "NextButton") as Button;
+            var previousButton = DependencyObjectEx.FindDescendant(calendarView, "PreviousButton") as Button;
 
-            var value = (bool)e.NewValue;
+            var showBorder = (bool)e.NewValue;
             calendarView.Loaded -= CalendarView_Loaded;
             calendarView.Loaded += CalendarView_Loaded;
 
-
             void CalendarView_Loaded(object sender, RoutedEventArgs e)
             {
-                if (headerBorder == null && topBorder == null)
+                if (headerBorder == null || topBorder == null || headerButton == null || nextButton == null || previousButton == null)
                 {
                     headerBorder = DependencyObjectEx.FindDescendant(calendarView, "PART_HeaderBorder");
                     topBorder = DependencyObjectEx.FindDescendant(calendarView, "PART_TopBorder");
+                    headerButton = DependencyObjectEx.FindDescendant(calendarView, "HeaderButton") as Button;
+                    nextButton = DependencyObjectEx.FindDescendant(calendarView, "NextButton") as Button;
+                    previousButton = DependencyObjectEx.FindDescendant(calendarView, "PreviousButton") as Button;
+
                     UpdateBorders();
                 }
             }
@@ -40,11 +46,11 @@ internal partial class CalendarViewAttach
             {
                 if (headerBorder != null)
                 {
-                    headerBorder.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+                    headerBorder.Visibility = showBorder ? Visibility.Visible : Visibility.Collapsed;
                 }
                 if (topBorder != null)
                 {
-                    if (value)
+                    if (showBorder)
                     {
                         topBorder.Height = 0;
                     }
@@ -52,6 +58,24 @@ internal partial class CalendarViewAttach
                     {
                         topBorder.Height = 1;
                     }
+                }
+
+                if (headerButton == null || nextButton == null || previousButton == null)
+                {
+                    return;
+                }
+
+                if (showBorder)
+                {
+                    headerButton.Style = Application.Current.Resources["AccentHeaderNavigationButtonStyle"] as Style;
+                    nextButton.Style = Application.Current.Resources["AccentNavigationButtonStyle"] as Style;
+                    previousButton.Style = Application.Current.Resources["AccentNavigationButtonStyle"] as Style;
+                }
+                else
+                {
+                    headerButton.Style = Application.Current.Resources["HeaderNavigationButtonStyle"] as Style;
+                    nextButton.Style = Application.Current.Resources["NavigationButtonStyle"] as Style;
+                    previousButton.Style = Application.Current.Resources["NavigationButtonStyle"] as Style;
                 }
             }
             UpdateBorders();
