5885d0c2792385a0c1db556b30ebd6dda3b39f4b
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-02-13 13:19:38 +0330

    Update App.xaml.cs

--- VSIX/DevWinUI_Template/WinUIApp-NavigationView/App.xaml.cs
+++ VSIX/DevWinUI_Template/WinUIApp-NavigationView/App.xaml.cs
@@ -13,7 +13,7 @@ public partial class App : Application
         NavService = new JsonNavigationService();
     }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected $OnLaunchedAsyncKeyword$override void OnLaunched(LaunchActivatedEventArgs args)
     {
         MainWindow = new MainWindow();
 
@@ -23,6 +23,6 @@ public partial class App : Application
         ThemeService = new ThemeService(MainWindow);
 
         MainWindow.Activate();$PackagedAppTaskId$$Windows11ContextMenuInitializer$$ConfigLogger$$UnhandeledException$
-    }$InitializeOtherMethods$
+    }
 }
 
