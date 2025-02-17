dadb1d6243b7b135d03e2da03f37f25eedaaab06
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-02-17 22:27:40 +0330

    Add BrushAttach, ColorBrightnessExtension and LightenColor/DarkenColor

---
+++ dev/DevWinUI/Extensions/ColorBrightnessExtension.cs
@@ -0,0 +1,46 @@
+﻿namespace DevWinUI;
+
+/// <summary>
+/// MarkupExtension to that can be used to brighten (positive) or darken (negative) a Color.
+/// <code>
+/// <Rectangle Width="40" Height="40">
+///     <Rectangle.Fill>
+///         <SolidColorBrush Color = "{helpers:ColorBrightness Color=Red, CorrectionFactor=-0.3}" />
+///     </ Rectangle.Fill >
+/// </ Rectangle >
+/// </code>
+/// </summary>
+[MarkupExtensionReturnType(ReturnType = typeof(Color))]
+public partial class ColorBrightnessExtension : MarkupExtension
+{
+
+    /// <summary>
+    /// Gets or sets the correction factor for the brightness adjustment.
+    /// Positive values brighten the color, negative values darken it.
+    /// Must be between -1 and 1.
+    /// </summary>
+    public double CorrectionFactor { get; set; }
+
+    /// <summary>
+    /// Gets or sets the color to be adjusted.
+    /// </summary>
+    public Color Color { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ColorBrightnessExtension"/> class.
+    /// </summary>
+    public ColorBrightnessExtension()
+    {
+    }
+
+    /// <summary>
+    /// Provides the value of the adjusted color.
+    /// </summary>
+    /// <returns>The adjusted <see cref="Color"/>.</returns>
+    protected override object ProvideValue()
+    {
+        return CorrectionFactor > 1
+            ? ColorHelper.LightenColor(Color, (float)CorrectionFactor)
+            : ColorHelper.DarkenColor(Color, - (float)CorrectionFactor);
+    }
+}
--- dev/DevWinUI/Helpers/ColorHelper.cs
+++ dev/DevWinUI/Helpers/ColorHelper.cs
@@ -60,4 +60,197 @@ public static partial class ColorHelper
         SolidColorBrush myBrush = new SolidColorBrush(Color.FromArgb(a, r, g, b));
         return myBrush;
     }
+
+    /// <summary>
+    /// Tints the color by the given percent.
+    /// </summary>
+    /// <param name="color">The color being tinted.</param>
+    /// <param name="percent">The percent to tint. Ex: 0.1 will make the color 10% lighter.</param>
+    /// <returns>The new tinted color.</returns>
+    public static Color LightenColor(Color color, float percent)
+    {
+        var lighting = GetBrightnessFromColor(color);
+        lighting = lighting + lighting * percent;
+        if (lighting > 1.0)
+        {
+            lighting = 1;
+        }
+        else if (lighting <= 0)
+        {
+            lighting = 0.1f;
+        }
+        var tintedColor = GetColorFromHsl(color.A, GetHueFromColor(color), GetSaturationFromColor(color), lighting);
+
+        return tintedColor;
+    }
+
+    /// <summary>
+    /// Tints the color by the given percent.
+    /// </summary>
+    /// <param name="color">The color being tinted.</param>
+    /// <param name="percent">The percent to tint. Ex: 0.1 will make the color 10% darker.</param>
+    /// <returns>The new tinted color.</returns>
+    public static Color DarkenColor(Color color, float percent)
+    {
+        var lighting = GetBrightnessFromColor(color);
+        lighting = lighting - lighting * percent;
+        if (lighting > 1.0)
+        {
+            lighting = 1;
+        }
+        else if (lighting <= 0)
+        {
+            lighting = 0;
+        }
+        var tintedColor = GetColorFromHsl(color.A, GetHueFromColor(color), GetSaturationFromColor(color), lighting);
+
+        return tintedColor;
+    }
+
+    /// <summary>
+    /// Converts the HSL values to a Color.
+    /// </summary>
+    /// <param name="alpha">The alpha.</param>
+    /// <param name="hue">The hue.</param>
+    /// <param name="saturation">The saturation.</param>
+    /// <param name="lighting">The lighting.</param>
+    /// <returns></returns>
+    public static Color GetColorFromHsl(byte alpha, float hue, float saturation, float lighting)
+    {
+        //if (alpha is < 0 or > 255) {
+        //    throw new ArgumentOutOfRangeException("alpha");
+        //}
+        if (hue is < 0f or > 360f)
+        {
+            throw new ArgumentOutOfRangeException("hue");
+        }
+        if (saturation is < 0f or > 1f)
+        {
+            throw new ArgumentOutOfRangeException("saturation");
+        }
+        if (lighting is < 0f or > 1f)
+        {
+            throw new ArgumentOutOfRangeException("lighting");
+        }
+
+        if (0 == saturation)
+        {
+            return Color.FromArgb(alpha, Convert.ToByte(lighting * 255), Convert.ToByte(lighting * 255), Convert.ToByte(lighting * 255));
+        }
+
+        float fMax, fMid, fMin;
+        int iSextant;
+
+        if (0.5 < lighting)
+        {
+            fMax = lighting - (lighting * saturation) + saturation;
+            fMin = lighting + (lighting * saturation) - saturation;
+        }
+        else
+        {
+            fMax = lighting + (lighting * saturation);
+            fMin = lighting - (lighting * saturation);
+        }
+
+        iSextant = (int)Math.Floor(hue / 60f);
+        if (300f <= hue)
+        {
+            hue -= 360f;
+        }
+        hue /= 60f;
+        hue -= 2f * (float)Math.Floor(((iSextant + 1f) % 6f) / 2f);
+        fMid = 0 == iSextant % 2
+            ? hue * (fMax - fMin) + fMin
+            : fMin - hue * (fMax - fMin);
+
+        byte bMax = Convert.ToByte(fMax * 255);
+        byte bMid = Convert.ToByte(fMid * 255);
+        byte bMin = Convert.ToByte(fMin * 255);
+
+        switch (iSextant)
+        {
+            case 1:
+                return Color.FromArgb(alpha, bMid, bMax, bMin);
+            case 2:
+                return Color.FromArgb(alpha, bMin, bMax, bMid);
+            case 3:
+                return Color.FromArgb(alpha, bMin, bMid, bMax);
+            case 4:
+                return Color.FromArgb(alpha, bMid, bMin, bMax);
+            case 5:
+                return Color.FromArgb(alpha, bMax, bMin, bMid);
+            default:
+                return Color.FromArgb(alpha, bMax, bMid, bMin);
+        }
+    }
+
+    /// <summary>
+    /// Gets the brightness of the color.
+    /// </summary>
+    /// <param name="color">The color.</param>
+    /// <returns>The brightness value.</returns>
+    public static float GetBrightnessFromColor(Color color)
+    {
+        return (color.R * 0.299f + color.G * 0.587f + color.B * 0.114f) / 255f;
+    }
+
+    /// <summary>
+    /// Gets the hue of the color.
+    /// </summary>
+    /// <param name="color">The color.</param>
+    /// <returns>The hue value.</returns>
+    public static float GetHueFromColor(Color color)
+    {
+        float r = color.R / 255f;
+        float g = color.G / 255f;
+        float b = color.B / 255f;
+
+        float max = Math.Max(r, Math.Max(g, b));
+        float min = Math.Min(r, Math.Min(g, b));
+
+        float hue = 0f;
+
+        if (max == min)
+        {
+            hue = 0f;
+        }
+        else if (max == r)
+        {
+            hue = (60f * ((g - b) / (max - min)) + 360f) % 360f;
+        }
+        else if (max == g)
+        {
+            hue = (60f * ((b - r) / (max - min)) + 120f) % 360f;
+        }
+        else if (max == b)
+        {
+            hue = (60f * ((r - g) / (max - min)) + 240f) % 360f;
+        }
+
+        return hue;
+    }
+
+    /// <summary>
+    /// Gets the saturation of the color.
+    /// </summary>
+    /// <param name="color">The color.</param>
+    /// <returns>The saturation value.</returns>
+    public static float GetSaturationFromColor(Color color)
+    {
+        float r = color.R / 255f;
+        float g = color.G / 255f;
+        float b = color.B / 255f;
+
+        float max = Math.Max(r, Math.Max(g, b));
+        float min = Math.Min(r, Math.Min(g, b));
+
+        float saturation = 0f;
+
+        if (max != 0)
+        {
+            saturation = (max - min) / max;
+        }
+
+        return saturation;
+    }
 }
