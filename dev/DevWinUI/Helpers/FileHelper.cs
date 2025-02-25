e4d8cb95dbf2a14efa07a47c9aeaaf32aadb6e62
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-02-26 01:37:34 +0330

    Add GetValidFileName for FileHelper

--- dev/DevWinUI/Helpers/FileHelper.cs
+++ dev/DevWinUI/Helpers/FileHelper.cs
@@ -1,6 +1,73 @@
-﻿namespace DevWinUI;
+﻿using System.Globalization;
+
+namespace DevWinUI;
 public partial class FileHelper
 {
+    private static readonly string[] ReservedFileNames =
+    [
+        "con", "prn", "aux", "nul",
+        "com0", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9",
+        "lpt0", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9",
+    ];
+    private static bool IsAllDots(string fileName)
+    {
+        foreach (var c in fileName)
+        {
+            if (c != '.')
+                return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Converts a text into a valid file name.
+    /// </summary>
+    /// <param name="fileName">The file name.</param>
+    /// <param name="reservedNameFormat">The reserved format to use for reserved names. If null '_{0}_' will be used.</param>
+    /// <param name="reservedCharFormat">The reserved format to use for reserved characters. If null '_x{0}_' will be used.</param>
+    /// <returns>
+    /// A valid file name.
+    /// </returns>
+    public static string GetValidFileName(string fileName, string reservedNameFormat = "_{0}_", string reservedCharFormat = "_x{0}_")
+    {
+        if (fileName is null)
+            throw new ArgumentNullException(nameof(fileName));
+
+        if (reservedNameFormat is null)
+            throw new ArgumentNullException(nameof(reservedNameFormat));
+
+        if (reservedCharFormat is null)
+            throw new ArgumentNullException(nameof(reservedCharFormat));
+        if (ReservedFileNames.Contains(fileName, StringComparer.OrdinalIgnoreCase) || IsAllDots(fileName))
+        {
+            return string.Format(CultureInfo.InvariantCulture, reservedNameFormat, fileName);
+        }
+
+        var invalid = Path.GetInvalidFileNameChars();
+
+        var sb = new StringBuilder(fileName.Length);
+        foreach (var c in fileName)
+        {
+            if (Array.IndexOf(invalid, c) >= 0)
+            {
+                sb.AppendFormat(CultureInfo.InvariantCulture, reservedCharFormat, (short)c);
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        var s = sb.ToString();
+        if (string.Equals(s, fileName, StringComparison.Ordinal))
+        {
+            s = fileName;
+        }
+
+        return s;
+    }
+   
     public static string GetFileSize(long size)
     {
         string[] sizeSuffixes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
