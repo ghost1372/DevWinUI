25fe950fefe3b0b3bc0d9ecc23a1965cc310bbec
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-07-16 00:19:28 +0330

    Add Logger

--- dev/DevWinUI.Gallery/Common/AppConfig.cs
+++ dev/DevWinUI.Gallery/Common/AppConfig.cs
@@ -6,12 +6,10 @@ namespace DevWinUIGallery.Common;
 [GenerateAutoSaveOnChange]
 public partial class AppConfig : NotifiyingJsonSettings, IVersionable
 {
-    [EnforcedVersion("1.0.0.0")]
-    public Version Version { get; set; } = new Version(1, 0, 0, 0);
+    [EnforcedVersion("8.5.0.0")]
+    public Version Version { get; set; } = new Version(8, 5, 0, 0);
 
     public string fileName { get; set; } = Constants.AppConfigPath;
-
+    private bool useDeveloperMode { get; set; }
     public string lastUpdateCheck { get; set; }
-
-    // Docs: https://github.com/Nucs/JsonSettings
 }
