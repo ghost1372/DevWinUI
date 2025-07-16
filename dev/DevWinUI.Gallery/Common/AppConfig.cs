7f9a859ee160124ce3ad354666c9d4b8b65508dd
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-07-16 22:24:38 +0330

    Update AppConfig.cs

--- dev/DevWinUI.Gallery/Common/AppConfig.cs
+++ dev/DevWinUI.Gallery/Common/AppConfig.cs
@@ -9,7 +9,7 @@ public partial class AppConfig : NotifiyingJsonSettings, IVersionable
     [EnforcedVersion("8.5.0.0")]
     public Version Version { get; set; } = new Version(8, 5, 0, 0);
 
-    public string fileName { get; set; } = Constants.AppConfigPath;
+    private string fileName { get; set; } = Constants.AppConfigPath;
     private bool useDeveloperMode { get; set; } = true;
-    public string lastUpdateCheck { get; set; }
+    private string lastUpdateCheck { get; set; }
 }
