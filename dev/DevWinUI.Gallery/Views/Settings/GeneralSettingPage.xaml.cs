764da275cace25307a7b9073c7e5a52f07f3e2a9
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-07-16 22:17:44 +0330

    Revert "Update GeneralSettingPage.xaml.cs"
    This reverts commit c8adc2b5bb38ead56a4ea71925f210d18862c9bd.

--- dev/DevWinUI.Gallery/Views/Settings/GeneralSettingPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Settings/GeneralSettingPage.xaml.cs
@@ -8,6 +8,16 @@ public sealed partial class GeneralSettingPage : Page
         ViewModel = App.GetService<GeneralSettingViewModel>();
         this.InitializeComponent();
     }
+
+    private async void NavigateToLogPath_Click(object sender, RoutedEventArgs e)
+    {
+        string folderPath = (sender as HyperlinkButton).Content.ToString();
+        if (Directory.Exists(folderPath))
+        {
+            Windows.Storage.StorageFolder folder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(folderPath);
+            await Windows.System.Launcher.LaunchFolderAsync(folder);
+        }
+    }
 }
 
 
