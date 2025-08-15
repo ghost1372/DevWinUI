5e1252bd93297be078eece64d3bd7b796239dbb0
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-08-15 12:10:30 +0330

    Update UnderlaySystemBackdropOptions.cs

--- dev/DevWinUI.Controls/Controls/WindowedContentDialog/Underlay/UnderlaySystemBackdropOptions.cs
+++ dev/DevWinUI.Controls/Controls/WindowedContentDialog/Underlay/UnderlaySystemBackdropOptions.cs
@@ -3,5 +3,5 @@ public sealed partial class UnderlaySystemBackdropOptions
 {
     public BackdropType Backdrop { get; set; } = BackdropType.Mica;
     public UnderlayCoverMode CoverMode { get; set; } = UnderlayCoverMode.ClientArea;
-    public ScalarTransition OpacityTransition { get; set; } = new ScalarTransition { Duration = TimeSpan.FromSeconds(0.25) };
+    public ScalarTransition OpacityTransition { get; set; } = new ScalarTransition { Duration = TimeSpan.FromSeconds(0.15) };
 }
