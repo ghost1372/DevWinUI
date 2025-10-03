8f31c4f02fca607e05793ebcb1191a59dd07eaca
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-10-03 12:35:44 +0330

    Add Some Properties in ProcessInfoHelper

--- dev/DevWinUI/Helpers/ProcessInfoHelper.cs
+++ dev/DevWinUI/Helpers/ProcessInfoHelper.cs
@@ -17,9 +17,40 @@ public static partial class ProcessInfoHelper
     public static string Version => GetVersion()?.ToString();
 
     /// <summary>
+    /// Returns "Dev" if compiled in Debug mode; otherwise, returns the version string.
+    /// </summary>
+    public static string VersionDev
+    {
+        get
+        {
+#if DEBUG
+            return "Dev";
+#else
+            return GetVersion()?.ToString();
+#endif
+        }
+    }
+
+    /// <summary>
     /// Returns the version string prefixed with 'v'.
     /// </summary>
     public static string VersionWithPrefix => $"v{Version}";
+
+    /// <summary>
+    /// Returns "Dev" if compiled in Debug mode; otherwise, returns the version string  prefixed with 'v'.
+    /// </summary>
+    public static string VersionWithPrefixDev
+    {
+        get
+        {
+#if DEBUG
+            return "Dev";
+#else
+            return $"v{Version}";
+#endif
+        }
+    }
+
 
     /// <summary>
     /// Retrieves the product name. If not available, it returns 'Unknown Product'.
@@ -27,9 +58,70 @@ public static partial class ProcessInfoHelper
     public static string ProductName => _fileVersionInfo?.ProductName ?? "Unknown Product";
 
     /// <summary>
+    /// Retrieves the product name with dots (.) replaced by spaces. 
+    /// If the product name is not available, returns "Unknown Product".
+    /// </summary>
+    public static string ProductNameDisplay => _fileVersionInfo?.ProductName?.Replace(".", " ") ?? "Unknown Product";
+
+    /// <summary>
+    /// Retrieves the product name. 
+    /// If compiled in Debug mode, appends "Dev" to indicate a development build. 
+    /// If the product name is not available, returns "Unknown Product".
+    /// </summary>
+    public static string ProductNameDev
+    {
+        get
+        {
+#if DEBUG
+            return ProductName + " Dev";
+#else
+            return ProductName;
+#endif
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the product name with dots (.) replaced by spaces. 
+    /// If compiled in Debug mode, appends "Dev" to indicate a development build. 
+    /// If the product name is not available, returns "Unknown Product".
+    /// </summary>
+    public static string ProductNameDisplayDev
+    {
+        get
+        {
+#if DEBUG
+            return ProductNameDisplay + " Dev";
+#else
+            return ProductNameDisplay;
+#endif
+        }
+    }
+
+
+    /// <summary>
     /// Combines the product name and version into a single string. The version includes a prefix.
     /// </summary>
     public static string ProductNameAndVersion => $"{ProductName} {VersionWithPrefix}";
+
+    /// <summary>
+    /// Combines the product name (with dots replaced by spaces) and version into a single string. 
+    /// The version includes a prefix. If the product name is not available, "Unknown Product" is used.
+    /// </summary>
+    public static string ProductNameAndVersionDisplay => $"{ProductNameDisplay} {VersionWithPrefix}";
+
+    /// <summary>
+    /// Combines the product name and version into a single string. 
+    /// If compiled in Debug mode, appends "Dev" to the product name to indicate a development build. 
+    /// The version includes a prefix. If the product name is not available, "Unknown Product" is used.
+    /// </summary>
+    public static string ProductNameAndVersionDev => $"{ProductNameDev} {VersionWithPrefix}";
+
+    /// <summary>
+    /// Combines the product name (with dots replaced by spaces) and version into a single string. 
+    /// If compiled in Debug mode, appends "Dev" to the product name to indicate a development build. 
+    /// The version includes a prefix. If the product name is not available, "Unknown Product" is used.
+    /// </summary>
+    public static string ProductNameAndVersionDisplayDev => $"{ProductNameDisplayDev} {VersionWithPrefix}";
 
     /// <summary>
     /// Returns the company name of the publisher. If not available, it defaults to 'Unknown Publisher'.
