67eaca9a3a389a2991e68829502cc851fa18e492
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-10-17 23:12:21 +0330

    Add SunTimesHelper

---
+++ dev/DevWinUI.Gallery/Views/Pages/Common/SunTimesHelperPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace DevWinUIGallery.Views;
+
+public sealed partial class SunTimesHelperPage : Page
+{
+    public SunTimesHelperPage()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnCalc_Click(object sender, RoutedEventArgs e)
+    {
+        var sunTimes = SunTimesHelper.CalculateSunriseSunset(NBLatitude.Value, NBLongitude.Value, DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        TxtSunRise.Text = $"Sunrise: {sunTimes.SunriseHour}:{sunTimes.SunriseMinute}";
+        TxtSunSet.Text = $"Sunset: {sunTimes.SunsetHour}:{sunTimes.SunsetMinute}";
+    }
+}
---
+++ dev/DevWinUI/Helpers/SunTimes/SunTimes.cs
@@ -0,0 +1,13 @@
+﻿namespace DevWinUI;
+
+public struct SunTimes
+{
+    public int SunriseHour;
+    public int SunriseMinute;
+    public int SunsetHour;
+    public int SunsetMinute;
+    public string Text;
+
+    public bool HasSunrise;
+    public bool HasSunset;
+}
