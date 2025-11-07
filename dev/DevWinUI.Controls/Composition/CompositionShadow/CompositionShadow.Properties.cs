4bf765ceaceb3d946a3683cdd72ddd38a71a6f93
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-11-07 22:20:30 +0330

    Add UseEdgeColorFromImage in CompositionShadow #122

---
+++ dev/DevWinUI.Controls/Composition/CompositionShadow/CompositionShadow.Properties.cs
@@ -0,0 +1,131 @@
+﻿namespace DevWinUI;
+
+public partial class CompositionShadow
+{
+    public double BlurRadius
+    {
+        get => (double)GetValue(BlurRadiusProperty);
+        set => SetValue(BlurRadiusProperty, value);
+    }
+    public static readonly DependencyProperty BlurRadiusProperty =
+        DependencyProperty.Register(nameof(BlurRadius), typeof(double), typeof(CompositionShadow), new PropertyMetadata(9.0, OnBlurRadiusChanged));
+    private static void OnBlurRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control._dropShadow.BlurRadius = (float)(double)e.NewValue;
+    }
+    public Color Color
+    {
+        get => (Color)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+    public static readonly DependencyProperty ColorProperty =
+        DependencyProperty.Register(nameof(Color), typeof(Color), typeof(CompositionShadow), new PropertyMetadata(Colors.Black, OnColorChanged));
+    private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control._dropShadow.Color = (Color)e.NewValue;
+    }
+    public double OffsetX
+    {
+        get => (double)GetValue(OffsetXProperty);
+        set => SetValue(OffsetXProperty, value);
+    }
+    public static readonly DependencyProperty OffsetXProperty =
+        DependencyProperty.Register(nameof(OffsetX), typeof(double), typeof(CompositionShadow), new PropertyMetadata(0.0, OnOffsetXChanged));
+    private static void OnOffsetXChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control.UpdateShadowOffset((float)(double)e.NewValue, control._dropShadow.Offset.Y, control._dropShadow.Offset.Z);
+    }
+    public double OffsetY
+    {
+        get => (double)GetValue(OffsetYProperty);
+        set => SetValue(OffsetYProperty, value);
+    }
+    public static readonly DependencyProperty OffsetYProperty =
+        DependencyProperty.Register(nameof(OffsetY), typeof(double), typeof(CompositionShadow), new PropertyMetadata(0.0, OnOffsetYChanged));
+    private static void OnOffsetYChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control.UpdateShadowOffset(control._dropShadow.Offset.X, (float)(double)e.NewValue, control._dropShadow.Offset.Z);
+    }
+    public double OffsetZ
+    {
+        get => (double)GetValue(OffsetZProperty);
+        set => SetValue(OffsetZProperty, value);
+    }
+    public static readonly DependencyProperty OffsetZProperty =
+        DependencyProperty.Register(nameof(OffsetZ), typeof(double), typeof(CompositionShadow), new PropertyMetadata(0.0, OnOffsetZChanged));
+    private static void OnOffsetZChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control.UpdateShadowOffset(control._dropShadow.Offset.X, control._dropShadow.Offset.Y, (float)(double)e.NewValue);
+    }
+    public double ShadowOpacity
+    {
+        get => (double)GetValue(ShadowOpacityProperty);
+        set => SetValue(ShadowOpacityProperty, value);
+    }
+    public static readonly DependencyProperty ShadowOpacityProperty =
+        DependencyProperty.Register(nameof(ShadowOpacity), typeof(double), typeof(CompositionShadow), new PropertyMetadata(1.0, OnShadowOpacityChanged));
+    private static void OnShadowOpacityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        control._dropShadow.Opacity = (float)(double)e.NewValue;
+    }
+    public FrameworkElement Content
+    {
+        get => (FrameworkElement)GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(nameof(Content), typeof(FrameworkElement), typeof(CompositionShadow), new PropertyMetadata(null, OnContentChanged));
+    private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (CompositionShadow)d;
+        if (e.OldValue is FrameworkElement oldElement)
+        {
+            oldElement.SizeChanged -= control.OnContentSizeChanged;
+        }
+        if (e.NewValue is FrameworkElement newElement)
+        {
+            newElement.SizeChanged += control.OnContentSizeChanged;
+        }
+        control.ConfigureShadowVisualForContent();
+    }
+
+    public bool UseEdgeColorFromImage
+    {
+        get => (bool)GetValue(UseEdgeColorFromImageProperty);
+        set => SetValue(UseEdgeColorFromImageProperty, value);
+    }
+
+    public static readonly DependencyProperty UseEdgeColorFromImageProperty =
+        DependencyProperty.Register(nameof(UseEdgeColorFromImage), typeof(bool), typeof(CompositionShadow), new PropertyMetadata(false, OnUseEdgeColorFromImageChanged));
+    private static async void OnUseEdgeColorFromImageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (CompositionShadow)d;
+        if (ctl != null)
+        {
+            await ctl.UpdateColorFromImageAsync();
+        }
+    }
+
+    public object ImageSourceForEdgeColor
+    {
+        get => (object)GetValue(ImageSourceForEdgeColorProperty);
+        set => SetValue(ImageSourceForEdgeColorProperty, value);
+    }
+
+    public static readonly DependencyProperty ImageSourceForEdgeColorProperty =
+        DependencyProperty.Register(nameof(ImageSourceForEdgeColor), typeof(object), typeof(CompositionShadow), new PropertyMetadata(null, OnImageSourceForEdgeColorChanged));
+    private static async void OnImageSourceForEdgeColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (CompositionShadow)d;
+        if (ctl != null)
+        {
+            await ctl.UpdateColorFromImageAsync();
+        }
+    }
+}
---
+++ dev/DevWinUI.Controls/Composition/CompositionShadow/CompositionShadow.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace DevWinUI;
+
+[ContentProperty(Name = nameof(Content))]
+[TemplatePart(Name = nameof(PART_ShadowElement), Type = typeof(Border))]
+public partial class CompositionShadow : Control
+{
+    private const string PART_ShadowElement = "PART_ShadowElement";
+    private Border _shadowElement;
+
+    private DropShadow _dropShadow;
+    private SpriteVisual _shadowVisual;
+    public DropShadow DropShadow => _dropShadow;
+
+    public SpriteVisual Visual => _shadowVisual;
+
+    public CompositionBrush Mask
+    {
+        get => _dropShadow?.Mask;
+        set
+        {
+            if (_dropShadow != null)
+            {
+                _dropShadow.Mask = value;
+            }
+        }
+    }
+    public CompositionShadow()
+    {
+        this.DefaultStyleKey = typeof(CompositionShadow);
+        this.SizeChanged += CompositionShadow_SizeChanged;
+        this.Loaded += CompositionShadow_Loaded;
+        var compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+
+        _shadowVisual = compositor.CreateSpriteVisual();
+        _dropShadow = compositor.CreateDropShadow();
+        _shadowVisual.Shadow = _dropShadow;
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        _shadowElement = GetTemplateChild(PART_ShadowElement) as Border;
+
+        if (_shadowElement != null)
+        {
+            ElementCompositionPreview.SetElementChildVisual(_shadowElement, _shadowVisual);
+        }
+
+        ConfigureShadowVisualForContent();
+    }
+
+    private void CompositionShadow_Loaded(object sender, RoutedEventArgs e)
+    {
+        ConfigureShadowVisualForContent();
+    }
+
+    private void OnContentSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        UpdateShadowSize();
+    }
+
+    private void CompositionShadow_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        UpdateShadowSize();
+    }
+
+    private void ConfigureShadowVisualForContent()
+    {
+        UpdateShadowMask();
+        UpdateShadowSize();
+
+        if (_dropShadow != null)
+        {
+            _dropShadow.BlurRadius = (float)BlurRadius;
+            _dropShadow.Color = Color;
+            _dropShadow.Opacity = (float)ShadowOpacity;
+            UpdateShadowOffset((float)OffsetX, (float)OffsetY, (float)OffsetZ);
+        }
+    }
+
+    private void UpdateShadowMask()
+    {
+        if (_dropShadow == null)
+            return;
+
+        if (Content != null)
+        {
+            CompositionBrush mask = null;
+            if (Content is Image image)
+            {
+                mask = image.GetAlphaMask();
+            }
+            else if (Content is Shape shape)
+            {
+                mask = shape.GetAlphaMask();
+            }
+            else if (Content is TextBlock textBlock)
+            {
+                mask = textBlock.GetAlphaMask();
+            }
+            _dropShadow.Mask = mask;
+        }
+        else
+        {
+            _dropShadow.Mask = null;
+        }
+    }
+
+    private void UpdateShadowOffset(float x, float y, float z)
+    {
+        if (_dropShadow != null)
+        {
+            _dropShadow.Offset = new Vector3(x, y, z);
+        }
+    }
+
+    private void UpdateShadowSize()
+    {
+        if (_shadowVisual == null)
+            return;
+
+        Vector2 newSize;
+        if (Content != null)
+        {
+            newSize = new Vector2((float)Content.ActualWidth, (float)Content.ActualHeight);
+        }
+        else
+        {
+            newSize = new Vector2((float)ActualWidth, (float)ActualHeight);
+        }
+
+        _shadowVisual.Size = newSize;
+    }
+
+    private async Task<Color> GetImageEdgeColorWithWin2DAsync(CanvasDevice device, Uri path)
+    {
+        using var bitmap = await CanvasBitmap.LoadAsync(device, path);
+        var colors = bitmap.GetPixelColors();
+
+        uint width = bitmap.SizeInPixels.Width;
+        uint height = bitmap.SizeInPixels.Height;
+
+        var edgePixels = new List<Color>();
+
+        for (int x = 0; x < width; x++)
+        {
+            edgePixels.Add(colors[x]);                    // top
+            edgePixels.Add(colors[(height - 1) * width + x]); // bottom
+        }
+        for (int y = 0; y < height; y++)
+        {
+            edgePixels.Add(colors[y * width]);            // left
+            edgePixels.Add(colors[y * width + (width - 1)]); // right
+        }
+
+        byte r = (byte)edgePixels.Average(c => c.R);
+        byte g = (byte)edgePixels.Average(c => c.G);
+        byte b = (byte)edgePixels.Average(c => c.B);
+
+        return Color.FromArgb(255, r, g, b);
+    }
+
+    private async Task UpdateColorFromImageAsync()
+    {
+        if (!UseEdgeColorFromImage || ImageSourceForEdgeColor == null)
+        {
+            Color = Colors.Black;
+            return;
+        }
+
+        Uri uri = null;
+
+        switch (ImageSourceForEdgeColor)
+        {
+            case Uri u:
+                uri = u;
+                break;
+
+            case string s when Uri.TryCreate(s, UriKind.RelativeOrAbsolute, out var uriFromString):
+                uri = uriFromString;
+                break;
+        }
+
+        if (uri != null)
+        {
+            var device = CanvasDevice.GetSharedDevice();
+            var color = await GetImageEdgeColorWithWin2DAsync(device, uri);
+            Color = color;
+        }
+        else
+        {
+            Color = Colors.Black;
+        }
+    }
+}
