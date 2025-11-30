94e351e865223bf933890b04b24afa0892d0b497
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-11-30 15:37:31 +0330

    Update BlendedImage Sample

--- dev/DevWinUI.Gallery/Views/Pages/Win2d/BlendedImagePage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Pages/Win2d/BlendedImagePage.xaml.cs
@@ -5,5 +5,8 @@ public sealed partial class BlendedImagePage : Page
     public BlendedImagePage()
     {
         InitializeComponent();
+
+        BlendedImageSample.PrimaryImageSource = new Uri(PathHelper.GetFilePath(new Uri("ms-appx:///Assets/Others/Image1.jpg")));
+        BlendedImageSample.SecondaryImageSource = new Uri(PathHelper.GetFilePath(new Uri("ms-appx:///Assets/Others/Image2.jpg")));
     }
 }
