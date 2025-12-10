fa8bb8dcd5bffb3833a3e1a789037c093677f014
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-12-10 17:28:10 +0330

    Fix LinearGradientBlurPanel Sample

--- dev/DevWinUI.Gallery/Views/Pages/Composition/LinearGradientBlurPanelPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Pages/Composition/LinearGradientBlurPanelPage.xaml.cs
@@ -1,24 +1,23 @@
-﻿using DevWinUI;
-using Windows.Foundation;
+﻿using Windows.Foundation;
 
 namespace DevWinUIGallery.Views;
 
 public sealed partial class LinearGradientBlurPanelPage : Page
 {
+    List<PointModel> PointList = new List<PointModel>()
+    {
+        new PointModel(new Point(0,0), new Point(0,1)),
+        new PointModel(new Point(0,0), new Point(1,1)),
+        new PointModel(new Point(0,0), new Point(1,0)),
+        new PointModel(new Point(0,1), new Point(1,0)),
+        new PointModel(new Point(0,1), new Point(0,0)),
+        new PointModel(new Point(1,1), new Point(0,0)),
+        new PointModel(new Point(1,0), new Point(0,0)),
+    };
+
     public LinearGradientBlurPanelPage()
     {
         InitializeComponent();
-
-        PointRadioButtons.ItemsSource = new[]
-        {
-            new PointModel(new Point(0,0), new Point(0,1)),
-            new PointModel(new Point(0,0), new Point(1,1)),
-            new PointModel(new Point(0,0), new Point(1,0)),
-            new PointModel(new Point(0,1), new Point(1,0)),
-            new PointModel(new Point(0,1), new Point(0,0)),
-            new PointModel(new Point(1,1), new Point(0,0)),
-            new PointModel(new Point(1,0), new Point(0,0)),
-        };
     }
     private void StartAnimationButton_Click(object sender, RoutedEventArgs e)
     {
@@ -43,6 +42,16 @@ public sealed partial class LinearGradientBlurPanelPage : Page
             ((Button)sender).IsEnabled = true;
         };
     }
+
+    private void PointRadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (PointRadioButtons.SelectedItem is PointModel pointModel)
+        {
+            LinearGradientBlurPanelSample.StartPoint = pointModel.StartPoint;
+            LinearGradientBlurPanelSample.EndPoint = pointModel.EndPoint;
+        }
+    }
+
     private record class PointModel(Point StartPoint, Point EndPoint)
     {
         public override string ToString()
