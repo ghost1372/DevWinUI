a1aff134913dabbfe83d73f6f0a5d261c4a68796
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-12-13 00:10:57 +0330

    Add Color and Intensity in AmbLight

--- dev/DevWinUI.Controls/Common/XamlLight/AmbLight.cs
+++ dev/DevWinUI.Controls/Common/XamlLight/AmbLight.cs
@@ -1,34 +1,70 @@
 ﻿namespace DevWinUI;
+
 public partial class AmbLight : XamlLight
 {
-    private static readonly string Id = typeof(AmbLight).FullName!;
+    private static readonly string Id = typeof(AmbLight).FullName;
+
+    public Color Color
+    {
+        get => (Color)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+    public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+        nameof(Color), typeof(Color), typeof(AmbLight), new PropertyMetadata(Colors.White, (s, e) =>
+        {
+            var self = (AmbLight)s;
+            var newColor = (Color)e.NewValue;
+
+            if (self.CompositionLight is AmbientLight ambientLight)
+            {
+                ambientLight.Color = newColor;
+            }
+        }));
+
+    public double Intensity
+    {
+        get => (double)GetValue(IntensityProperty);
+        set => SetValue(IntensityProperty, value);
+    }
+    public static readonly DependencyProperty IntensityProperty = DependencyProperty.Register(
+        nameof(Intensity), typeof(double), typeof(AmbLight), new PropertyMetadata(1.0d, (s, e) =>
+        {
+            var self = (AmbLight)s;
+            var newIntensity = (float)e.NewValue;
+
+            if (self.CompositionLight is AmbientLight ambientLight)
+            {
+                ambientLight.Intensity = newIntensity;
+            }
+        }));
 
     protected override void OnConnected(UIElement newElement)
     {
-        Compositor compositor = CompositionTarget.GetCompositorForCurrentThread();
+        var compositor = CompositionTarget.GetCompositorForCurrentThread();
 
-        // Create AmbientLight and set its properties
-        AmbientLight ambientLight = compositor.CreateAmbientLight();
-        ambientLight.Color = Colors.White;
-
-        // Associate CompositionLight with XamlLight
+        var ambientLight = CreateAmbientLight();
         CompositionLight = ambientLight;
 
-        // Add UIElement to the Light's Targets
         AddTargetElement(GetId(), newElement);
+
+        AmbientLight CreateAmbientLight()
+        {
+            var light = compositor.CreateAmbientLight();
+
+            light.Color = Color;
+            light.Intensity = Intensity.ToFloat();
+
+            return light;
+        }
     }
 
     protected override void OnDisconnected(UIElement oldElement)
     {
-        // Dispose Light when it is removed from the tree
         RemoveTargetElement(GetId(), oldElement);
         CompositionLight.Dispose();
     }
 
-    protected override string GetId()
-    {
-        return Id;
-    }
+    protected override string GetId() => Id;
 }
 
 
