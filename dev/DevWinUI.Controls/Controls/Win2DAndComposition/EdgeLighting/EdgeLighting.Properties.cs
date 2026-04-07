29c231dd1b2bce576915375e82d4c77fcf48783f
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-07 17:04:50 +0330

    Add EdgeLighting

---
+++ dev/DevWinUI.Controls/Controls/Win2DAndComposition/EdgeLighting/EdgeLighting.Properties.cs
@@ -0,0 +1,272 @@
+﻿namespace DevWinUI;
+
+public partial class EdgeLighting
+{
+    public bool IsLighting
+    {
+        get { return (bool)GetValue(IsLightingProperty); }
+        set { SetValue(IsLightingProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsLightingProperty =
+        DependencyProperty.Register(nameof(IsLighting), typeof(bool), typeof(EdgeLighting), new PropertyMetadata(true, OnIsLightingChanged));
+
+    private static void OnIsLightingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateLightingState();
+        }
+    }
+
+    public double StrokeThickness
+    {
+        get { return (double)GetValue(StrokeThicknessProperty); }
+        set { SetValue(StrokeThicknessProperty, value); }
+    }
+
+    public static readonly DependencyProperty StrokeThicknessProperty =
+        DependencyProperty.Register(nameof(StrokeThickness), typeof(double), typeof(EdgeLighting), new PropertyMetadata(10d, OnStrokeThicknessChanged));
+
+    private static void OnStrokeThicknessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateStrokeThickness();
+        }
+    }
+
+    public new double CornerRadius
+    {
+        get { return (double)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+
+    public static new readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(nameof(CornerRadius), typeof(double), typeof(EdgeLighting), new PropertyMetadata(8.0d, OnCornerRadiusChanged));
+
+    private static void OnCornerRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.Build();
+            ctl.UpdateBorderCornerRadius();
+        }
+    }
+    public Color BorderStrokeColor
+    {
+        get { return (Color)GetValue(BorderStrokeColorProperty); }
+        set { SetValue(BorderStrokeColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty BorderStrokeColorProperty =
+        DependencyProperty.Register(nameof(BorderStrokeColor), typeof(Color), typeof(EdgeLighting), new PropertyMetadata(Windows.UI.Color.FromArgb(60, 20, 20, 20), OnBorderBackgroundChanged));
+
+    private static void OnBorderBackgroundChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBorderBackground();
+        }
+    }
+
+    public Color HighlightColor
+    {
+        get { return (Color)GetValue(HighlightColorProperty); }
+        set { SetValue(HighlightColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty HighlightColorProperty =
+        DependencyProperty.Register(nameof(HighlightColor), typeof(Color), typeof(EdgeLighting), new PropertyMetadata(Windows.UI.Color.FromArgb(255, 255, 255, 255), OnHighlightColorChanged));
+
+    private static void OnHighlightColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public double HighlightOffset
+    {
+        get { return (double)GetValue(HighlightOffsetProperty); }
+        set { SetValue(HighlightOffsetProperty, value); }
+    }
+
+    public static readonly DependencyProperty HighlightOffsetProperty =
+        DependencyProperty.Register(nameof(HighlightOffset), typeof(double), typeof(EdgeLighting), new PropertyMetadata(0.0d, OnHighlightOffsetChanged));
+
+    private static void OnHighlightOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public Color FadeColor
+    {
+        get { return (Color)GetValue(FadeColorProperty); }
+        set { SetValue(FadeColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty FadeColorProperty =
+        DependencyProperty.Register(nameof(FadeColor), typeof(Color), typeof(EdgeLighting), new PropertyMetadata(Windows.UI.Color.FromArgb(0, 0, 120, 215), OnFadeColorChanged));
+
+    private static void OnFadeColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public double FadeOffset
+    {
+        get { return (double)GetValue(FadeOffsetProperty); }
+        set { SetValue(FadeOffsetProperty, value); }
+    }
+
+    public static readonly DependencyProperty FadeOffsetProperty =
+        DependencyProperty.Register(nameof(FadeOffset), typeof(double), typeof(EdgeLighting), new PropertyMetadata(0.4d, OnFadeOffsetChanged));
+
+    private static void OnFadeOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public Color MidColor
+    {
+        get { return (Color)GetValue(MidColorProperty); }
+        set { SetValue(MidColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty MidColorProperty =
+        DependencyProperty.Register(nameof(MidColor), typeof(Color), typeof(EdgeLighting), new PropertyMetadata(Windows.UI.Color.FromArgb(255, 0, 120, 215), OnMidColorChanged));
+
+    private static void OnMidColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public double MidOffset
+    {
+        get { return (double)GetValue(MidOffsetProperty); }
+        set { SetValue(MidOffsetProperty, value); }
+    }
+
+    public static readonly DependencyProperty MidOffsetProperty =
+        DependencyProperty.Register(nameof(MidOffset), typeof(double), typeof(EdgeLighting), new PropertyMetadata(0.3d, OnMidOffsetChanged));
+    private static void OnMidOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+
+    public Color TailColor
+    {
+        get { return (Color)GetValue(TailColorProperty); }
+        set { SetValue(TailColorProperty, value); }
+    }
+
+    public static readonly DependencyProperty TailColorProperty =
+        DependencyProperty.Register(nameof(TailColor), typeof(Color), typeof(EdgeLighting), new PropertyMetadata(Colors.Transparent, OnTailColorChanged));
+
+    private static void OnTailColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.UpdateBrush();
+        }
+    }
+    public TimeSpan Duration
+    {
+        get { return (TimeSpan)GetValue(DurationProperty); }
+        set { SetValue(DurationProperty, value); }
+    }
+
+
+    public static readonly DependencyProperty DurationProperty =
+        DependencyProperty.Register(nameof(Duration), typeof(TimeSpan), typeof(EdgeLighting), new PropertyMetadata(TimeSpan.FromSeconds(4), OnDurationChanged));
+
+    private static void OnDurationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.Build();
+        }
+    }
+
+    public int IterationCount
+    {
+        get { return (int)GetValue(IterationCountProperty); }
+        set { SetValue(IterationCountProperty, value); }
+    }
+
+    public static readonly DependencyProperty IterationCountProperty =
+        DependencyProperty.Register(nameof(IterationCount), typeof(int), typeof(EdgeLighting), new PropertyMetadata(1, OnIterationCountChanged));
+
+    private static void OnIterationCountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.Build();
+        }
+    }
+
+    public AnimationIterationBehavior IterationBehavior
+    {
+        get { return (AnimationIterationBehavior)GetValue(IterationBehaviorProperty); }
+        set { SetValue(IterationBehaviorProperty, value); }
+    }
+
+    public static readonly DependencyProperty IterationBehaviorProperty =
+        DependencyProperty.Register(nameof(IterationBehavior), typeof(AnimationIterationBehavior), typeof(EdgeLighting), new PropertyMetadata(AnimationIterationBehavior.Forever, OnIterationBehaviorChanged));
+    private static void OnIterationBehaviorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.Build();
+        }
+    }
+    public bool IsRounded
+    {
+        get { return (bool)GetValue(IsRoundedProperty); }
+        set { SetValue(IsRoundedProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsRoundedProperty =
+        DependencyProperty.Register(nameof(IsRounded), typeof(bool), typeof(EdgeLighting), new PropertyMetadata(false, IsRoundedChanged));
+
+    private static void IsRoundedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctl = (EdgeLighting)d;
+        if (ctl != null)
+        {
+            ctl.Build();
+            ctl.UpdateBorderCornerRadius();
+        }
+    }
+}
---
+++ dev/DevWinUI.Controls/Controls/Win2DAndComposition/EdgeLighting/EdgeLighting.cs
@@ -0,0 +1,192 @@
+﻿namespace DevWinUI;
+
+[TemplatePart(Name = nameof(PART_Border), Type = typeof(Grid))]
+public partial class EdgeLighting : ContentControl
+{
+    private const string PART_Border = "PART_Border";
+
+    private Border border;
+
+    private Compositor _compositor;
+    private CompositionRadialGradientBrush brush;
+    private ExpressionAnimation expression;
+    private CompositionPropertySet props;
+    private ScalarKeyFrameAnimation? progressAnimation;
+    private CompositionSpriteShape bgShape;
+    private CompositionSpriteShape glowShape;
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        border = GetTemplateChild(PART_Border) as Border;
+
+        SizeChanged -= OnSizeChanged;
+        SizeChanged += OnSizeChanged;
+    }
+
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        Build();
+        UpdateBorderCornerRadius();
+    }
+
+    private void UpdateBorderBackground()
+    {
+        if (bgShape == null || _compositor == null)
+            return;
+
+        bgShape.StrokeBrush = _compositor.CreateColorBrush(BorderStrokeColor);
+    }
+    private void UpdateStrokeThickness()
+    {
+        if (bgShape == null || glowShape == null || _compositor == null)
+            return;
+
+        bgShape.StrokeThickness = (float)StrokeThickness;
+        glowShape.StrokeThickness = (float)StrokeThickness;
+
+    }
+    private void Build()
+    {
+        if (border == null)
+            return;
+
+        _compositor = Microsoft.UI.Xaml.Hosting.ElementCompositionPreview.GetElementVisual(border).Compositor;
+
+        float width = (float)border.ActualWidth;
+        float height = (float)border.ActualHeight;
+
+        // Geometry        
+        CompositionRoundedRectangleGeometry roundedRectangleGeometry = null;
+        CompositionEllipseGeometry ellipseGeometry = null;
+        if (IsRounded)
+        {
+            float radius = Math.Min(width, height) / 2f;
+            ellipseGeometry = _compositor.CreateEllipseGeometry();
+            ellipseGeometry.Center = new Vector2(width / 2f, height / 2f);
+            ellipseGeometry.Radius = new Vector2(radius, radius);
+
+            bgShape = _compositor.CreateSpriteShape(ellipseGeometry);
+            glowShape = _compositor.CreateSpriteShape(ellipseGeometry);
+        }
+        else
+        {
+            roundedRectangleGeometry = _compositor.CreateRoundedRectangleGeometry();
+            roundedRectangleGeometry.Size = new System.Numerics.Vector2(width, height);
+            roundedRectangleGeometry.CornerRadius = new System.Numerics.Vector2((float)CornerRadius);
+
+            bgShape = _compositor.CreateSpriteShape(roundedRectangleGeometry);
+            glowShape = _compositor.CreateSpriteShape(roundedRectangleGeometry);
+        }
+
+        // Background border
+        bgShape.StrokeBrush = _compositor.CreateColorBrush(BorderStrokeColor);
+        bgShape.StrokeThickness = (float)StrokeThickness;
+        bgShape.FillBrush = null;
+
+        // Glow brush
+        brush = _compositor.CreateRadialGradientBrush();
+        brush.MappingMode = CompositionMappingMode.Absolute;
+
+        brush.EllipseRadius = new System.Numerics.Vector2(width, height);
+
+        UpdateBrush();
+
+        // Glow shape
+        glowShape.StrokeBrush = brush;
+        glowShape.StrokeThickness = (float)StrokeThickness;
+        glowShape.FillBrush = null;
+
+        // Shape visual
+        var shapeVisual = _compositor.CreateShapeVisual();
+        shapeVisual.Size = new System.Numerics.Vector2(width, height);
+
+        shapeVisual.Shapes.Add(bgShape);
+        shapeVisual.Shapes.Add(glowShape);
+
+        // Clip
+        var clipGeo = _compositor.CreateRoundedRectangleGeometry();
+        clipGeo.Size = new System.Numerics.Vector2(width, height);
+        clipGeo.CornerRadius = new System.Numerics.Vector2((float)CornerRadius);
+
+        shapeVisual.Clip = _compositor.CreateGeometricClip(clipGeo);
+
+        Microsoft.UI.Xaml.Hosting.ElementCompositionPreview.SetElementChildVisual(border, shapeVisual);
+
+        props = _compositor.CreatePropertySet();
+        props.InsertScalar("progress", 0f);
+
+        progressAnimation = _compositor.CreateScalarKeyFrameAnimation();
+
+        // Linear easing
+        var linear = _compositor.CreateLinearEasingFunction();
+        progressAnimation.InsertKeyFrame(1f, (float)(Math.PI * 2), linear);
+        progressAnimation.Duration = Duration;
+        progressAnimation.IterationBehavior = IterationBehavior;
+        progressAnimation.IterationCount = IterationCount;
+
+        float radiusParameter = Math.Min(width, height) / 2;
+
+        expression = _compositor.CreateExpressionAnimation(
+            "Vector2(cx + cos(props.progress) * r, cy + sin(props.progress) * r)");
+
+        expression.SetReferenceParameter("props", props);
+        expression.SetScalarParameter("cx", width / 2);
+        expression.SetScalarParameter("cy", height / 2);
+        expression.SetScalarParameter("r", radiusParameter);
+
+        UpdateLightingState();
+    }
+    private void UpdateBrush()
+    {
+        if (brush == null || _compositor == null)
+            return;
+
+        var highlight = _compositor.CreateColorGradientStop((float)HighlightOffset, HighlightColor);
+        var mid = _compositor.CreateColorGradientStop((float)MidOffset, MidColor);
+        var fade = _compositor.CreateColorGradientStop((float)FadeOffset, FadeColor);
+        var transparent = _compositor.CreateColorGradientStop(1f, TailColor);
+
+        brush.ColorStops.Clear();
+        brush.ColorStops.Add(highlight);
+        brush.ColorStops.Add(mid);
+        brush.ColorStops.Add(fade);
+        brush.ColorStops.Add(transparent);
+    }
+    private void UpdateLightingState()
+    {
+        if (brush == null || expression == null || props == null || progressAnimation == null)
+            return;
+
+        if (IsLighting)
+        {
+            props.StartAnimation("progress", progressAnimation);
+
+            brush.StartAnimation(nameof(brush.CenterPoint), expression);
+            brush.StartAnimation(nameof(brush.EllipseCenter), expression);
+        }
+        else
+        {
+            props.StopAnimation("progress");
+
+            brush.StopAnimation(nameof(brush.CenterPoint));
+            brush.StopAnimation(nameof(brush.EllipseCenter));
+        }
+    }
+
+    private void UpdateBorderCornerRadius()
+    {
+        if (border == null)
+            return;
+
+        if (IsRounded)
+        {
+            double gridRadius = Math.Min(border.ActualWidth, border.ActualHeight) / 2;
+            border.CornerRadius = new Microsoft.UI.Xaml.CornerRadius(gridRadius);
+        }
+        else
+        {
+            border.CornerRadius = new CornerRadius(CornerRadius);
+        }
+    }
+}
