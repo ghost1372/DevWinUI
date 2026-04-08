951f1900a472b55a534bd2654e11ee2b9799ce16
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-08 15:58:18 +0330

    Get WASDK Version Dynamically

--- dev/DevWinUI.Gallery/Views/HomeLandingPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/HomeLandingPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿namespace DevWinUIGallery.Views;
+﻿using Microsoft.Windows.ApplicationModel.WindowsAppRuntime;
+
+namespace DevWinUIGallery.Views;
 
 public sealed partial class HomeLandingPage : Page
 {
+    public string WASDKVersion { get; } = $"Windows App SDK {ReleaseInfo.Major}.{ReleaseInfo.Minor}+";
     public HomeLandingPage()
     {
         this.InitializeComponent();
