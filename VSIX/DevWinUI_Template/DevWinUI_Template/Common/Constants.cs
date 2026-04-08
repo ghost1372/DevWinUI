2aa7ed38ffa798978dc436e67e47ab4a49f30b66
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-08 11:51:05 +0330

    Update VSIX

--- VSIX/DevWinUI_Template/DevWinUI_Template/Common/Constants.cs
+++ VSIX/DevWinUI_Template/DevWinUI_Template/Common/Constants.cs
@@ -2,9 +2,9 @@
 
 public static class Constants
 {
-    internal readonly static string DevWinUI_Core = "DevWinUI";
-    internal readonly static string DevWinUI_Controls = "DevWinUI.Controls";
+    internal readonly static string DevWinUI_Core = "DevWinUI.Base";
+    internal readonly static string DevWinUI_Controls = "DevWinUI";
     internal readonly static string DevWinUI_ContextMenu = "DevWinUI.ContextMenu";
 
-    internal readonly static string DevWinUI_Controls_Xaml = "<ResourceDictionary Source=\"ms-appx:///DevWinUI.Controls/Themes/Generic.xaml\" />";
+    internal readonly static string DevWinUI_SourceGenerator = "DevWinUI.SourceGenerator";
 }
