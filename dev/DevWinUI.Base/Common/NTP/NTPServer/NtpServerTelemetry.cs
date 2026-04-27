65af840989530cf2498dfa0ce0c91889d4a448b8
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-27 13:24:10 +0330

    Add NTPServer and NTPClient

---
+++ dev/DevWinUI.Base/Common/NTP/NTPServer/NtpServerTelemetry.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+namespace DevWinUI;
+
+internal static partial class NtpServerTelemetry
+{
+    public static readonly ActivitySource ActivitySource = new("DevWinUI");
+}
