db209298aadda93e6e072e019e6be5c061f0fb87
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-28 23:10:10 +0330

    Update IsEnabledTextBlock

--- dev/DevWinUI/Controls/Native/Others/IsEnabledTextBlock.cs
+++ dev/DevWinUI/Controls/Native/Others/IsEnabledTextBlock.cs
@@ -2,6 +2,7 @@
 
 namespace DevWinUI;
 
+
 [TemplateVisualState(Name = "Normal", GroupName = "CommonStates")]
 [TemplateVisualState(Name = "Disabled", GroupName = "CommonStates")]
 public partial class IsEnabledTextBlock : Control
@@ -19,17 +20,21 @@ public partial class IsEnabledTextBlock : Control
         base.OnApplyTemplate();
     }
 
-    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
-       "Text",
-       typeof(string),
-       typeof(IsEnabledTextBlock),
-       null);
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(IsEnabledTextBlock), new PropertyMetadata(null));
 
     [Localizable(true)]
     public string Text
     {
         get => (string)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
+    }
+
+    public static readonly DependencyProperty IsTextSelectionEnabledProperty = DependencyProperty.Register(nameof(IsTextSelectionEnabled), typeof(bool), typeof(IsEnabledTextBlock), new PropertyMetadata(false));
+
+    public bool IsTextSelectionEnabled
+    {
+        get => (bool)GetValue(IsTextSelectionEnabledProperty);
+        set => SetValue(IsTextSelectionEnabledProperty, value);
     }
 
     private void IsEnabledTextBlock_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
