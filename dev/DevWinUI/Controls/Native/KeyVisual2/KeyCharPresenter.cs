69a4e9ae4242c92f65f3ca27ca62a21f7a87eabb
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-04-28 23:42:02 +0330

    Add KeyVisual2

---
+++ dev/DevWinUI/Controls/Native/KeyVisual2/KeyCharPresenter.cs
@@ -0,0 +1,17 @@
+﻿namespace DevWinUI;
+
+public sealed partial class KeyCharPresenter : Control
+{
+    public KeyCharPresenter()
+    {
+        DefaultStyleKey = typeof(KeyCharPresenter);
+    }
+
+    public object Content
+    {
+        get => (object)GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+
+    public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content), typeof(object), typeof(KeyCharPresenter), new PropertyMetadata(default(string)));
+}
