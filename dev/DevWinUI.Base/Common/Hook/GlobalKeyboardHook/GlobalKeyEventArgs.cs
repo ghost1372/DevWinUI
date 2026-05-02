fc7ee9cb951f1cdca468dc42a98a1b2165fdfb51
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-05-02 14:52:38 +0330

    Add GlobalKeyboardHook

---
+++ dev/DevWinUI.Base/Common/Hook/GlobalKeyboardHook/GlobalKeyEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using Windows.System;
+
+namespace DevWinUI;
+
+public sealed partial class GlobalKeyEventArgs : EventArgs
+{
+    const int KEY_DOWN_MASK = 0x8000;
+    public bool IsKeyDown(VirtualKey key)
+    {
+        return (PInvoke.GetKeyState((int)key) & KEY_DOWN_MASK) != 0;
+    }
+
+    public bool IsShift =>
+        IsKeyDown(VirtualKey.Shift) ||
+        IsKeyDown(VirtualKey.LeftShift) ||
+        IsKeyDown(VirtualKey.RightShift);
+
+    public bool IsCtrl =>
+        IsKeyDown(VirtualKey.Control) ||
+        IsKeyDown(VirtualKey.LeftControl) ||
+        IsKeyDown(VirtualKey.RightControl);
+
+    public bool IsAlt =>
+        IsKeyDown(VirtualKey.Menu) ||
+        IsKeyDown(VirtualKey.LeftMenu) ||
+        IsKeyDown(VirtualKey.RightMenu);
+
+    public bool IsWindows =>
+        IsKeyDown(VirtualKey.LeftWindows) ||
+        IsKeyDown(VirtualKey.RightWindows);
+    public VirtualKey Key { get; }
+
+    public GlobalKeyEventArgs(VirtualKey key)
+    {
+        Key = key;
+    }
+}
---
+++ dev/DevWinUI.Base/Common/Hook/GlobalKeyboardHook/GlobalKeyboardHook.cs
@@ -0,0 +1,116 @@
+﻿using Windows.System;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace DevWinUI;
+
+public sealed partial class GlobalKeyboardHook : IDisposable
+{
+    public event EventHandler<GlobalKeyEventArgs>? KeyDown;
+    public event EventHandler<GlobalKeyEventArgs>? KeyUp;
+    public event EventHandler<GlobalKeyEventArgs>? KeyPressed;
+
+    private Thread? _thread;
+    private HHOOK _hook;
+    private uint _threadId;
+    private bool _running;
+    private UnhookWindowsHookExSafeHandle? _hookId;
+    private static GlobalKeyboardHook Current { get; set; }
+
+    public GlobalKeyboardHook()
+    {
+        if (Current == null)
+        {
+            Current = this;
+        }
+    }
+
+    public void Start()
+    {
+        if (_running)
+            return;
+
+        _running = true;
+
+        _thread = new Thread(ThreadProc)
+        {
+            IsBackground = true,
+        };
+
+        _thread.Start();
+    }
+
+    public void Stop()
+    {
+        if (!_running)
+            return;
+
+        _running = false;
+
+        if (_threadId != 0)
+        {
+            PInvoke.PostThreadMessage(_threadId, PInvoke.WM_QUIT, 0, 0);
+        }
+
+        _thread?.Join();
+        _thread = null;
+    }
+
+    private unsafe void ThreadProc()
+    {
+        _threadId = PInvoke.GetCurrentThreadId();
+
+        delegate* unmanaged[Stdcall]<int, WPARAM, LPARAM, LRESULT> callback = &HookCallback;
+
+        _hook = PInvoke.SetWindowsHookEx(WINDOWS_HOOK_ID.WH_KEYBOARD_LL, callback, HINSTANCE.Null, 0);
+
+        _hookId = new UnhookWindowsHookExSafeHandle(_hook);
+
+        if (_hook.IsNull)
+            throw new InvalidOperationException("Failed to install keyboard hook.");
+
+        MSG msg;
+        while (PInvoke.GetMessage(out msg, HWND.Null, 0, 0))
+        {
+            PInvoke.TranslateMessage(msg);
+            PInvoke.DispatchMessage(msg);
+        }
+
+        PInvoke.UnhookWindowsHookEx(_hook);
+        _hook = default;
+    }
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+    private static LRESULT HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+    {
+        if (nCode >= 0)
+        {
+            var data = System.Runtime.InteropServices.Marshal
+               .PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
+
+            var key = (VirtualKey)data.vkCode;
+
+            var args = new GlobalKeyEventArgs(key);
+
+            switch ((uint)wParam.Value)
+            {
+                case PInvoke.WM_KEYDOWN:
+                case PInvoke.WM_SYSKEYDOWN:
+                    Current?.KeyDown?.Invoke(Current, args);
+                    Current?.KeyPressed?.Invoke(Current, args);
+                    break;
+
+                case PInvoke.WM_KEYUP:
+                case PInvoke.WM_SYSKEYUP:
+                    Current?.KeyUp?.Invoke(Current, args);
+                    break;
+            }
+        }
+
+        return PInvoke.CallNextHookEx(Current?._hookId, nCode, wParam, lParam);
+    }
+
+    public void Dispose()
+    {
+        Stop();
+    }
+}
