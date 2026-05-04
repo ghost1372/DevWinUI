c79424c15f022466e661133f3d57e9c78ad247a0
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-05-04 19:44:53 +0330

    Add EyeDropper Sample

---
+++ dev/DevWinUI.Gallery/Views/Pages/Common/EyeDropperPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml.Media;
+
+namespace DevWinUIGallery.Views;
+
+public sealed partial class EyeDropperPage : Page
+{
+    public EyeDropperPage()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnPick_Click(object sender, RoutedEventArgs e)
+    {
+        var eyeDropper = new EyeDropper();
+        eyeDropper.Start();
+        eyeDropper.ColorPicked += EyeDropper_ColorPicked;
+    }
+
+    private void EyeDropper_ColorPicked(object sender, Windows.UI.Color e)
+    {
+        DispatcherQueue.TryEnqueue(() =>
+        {
+            PreviewBox.Background = new SolidColorBrush(e);
+            TxtHex.Text = $"Color picked: {DevWinUI.ColorHelper.GetHexFromColor(e)}";
+        });
+    }
+}
