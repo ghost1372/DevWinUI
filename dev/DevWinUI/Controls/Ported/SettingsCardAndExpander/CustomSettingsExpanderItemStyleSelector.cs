64bca79c774df9c6bf2ec17337a7d45f498e519f
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-05-22 16:29:30 +0330

    Add Support Nested SettingsExpander

---
+++ dev/DevWinUI/Controls/Ported/SettingsCardAndExpander/CustomSettingsExpanderItemStyleSelector.cs
@@ -0,0 +1,19 @@
+﻿namespace DevWinUI;
+
+internal sealed partial class CustomSettingsExpanderItemStyleSelector : SettingsExpanderItemStyleSelector
+{
+    public Style ExpanderStyle { get; set; }
+
+    protected override Style SelectStyleCore(object item, DependencyObject container)
+    {
+        switch (container)
+        {
+            case SettingsCard:
+                return base.SelectStyleCore(item, container);
+            case SettingsExpander:
+                return ExpanderStyle;
+        }
+
+        return null;
+    }
+}
