c01a3290055021a0d14be14ff92ffca09a068a2b
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-06-02 01:58:56 +0330

    Add BoolNegationConverter

---
+++ dev/DevWinUI/Converters/BoolNegationConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace DevWinUI;
+
+public partial class BoolNegationConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return !(value is bool && (bool)value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return !(value is bool && (bool)value);
+    }
+}
