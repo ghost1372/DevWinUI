7f4a8062320c87e9583b711cb104eaf6377e5bd4
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-06-11 15:19:32 +0330

    Update HomeLandingPage.xaml.cs

--- dev/DevWinUI.Gallery/Views/HomeLandingPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/HomeLandingPage.xaml.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Windows.ApplicationModel.WindowsAppRuntime;
-
-namespace DevWinUIGallery.Views;
+﻿namespace DevWinUIGallery.Views;
 
 public sealed partial class HomeLandingPage : Page
 {
-    public string WASDKVersion { get; } = $"Windows App SDK 1.8+";
+    public string WASDKVersion { get; } = $"Windows App SDK 2.2+";
     public HomeLandingPage()
     {
         this.InitializeComponent();
