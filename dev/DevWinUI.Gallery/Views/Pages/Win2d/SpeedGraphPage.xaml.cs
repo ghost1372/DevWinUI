cda20e291e92f49b3fc2adfc0f14ccfc7856ad65
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-07-05 13:56:31 +0330

    Add SpeedGraph2 Sample

--- dev/DevWinUI.Gallery/Views/Pages/Win2d/SpeedGraphPage.xaml.cs
+++ dev/DevWinUI.Gallery/Views/Pages/Win2d/SpeedGraphPage.xaml.cs
@@ -5,7 +5,9 @@ public sealed partial class SpeedGraphPage : Page
     public ObservableCollection<SpeedGraphBackgroundMode> SpeedGraphBackgroundModeItems { get; set; } = new ObservableCollection<SpeedGraphBackgroundMode>(Enum.GetValues<SpeedGraphBackgroundMode>());
 
     private bool _isSimulating;
+    private bool _isSimulating2;
     private Random _random = new();
+    private Random _random2 = new();
     public SpeedGraphPage()
     {
         InitializeComponent();
@@ -62,5 +64,55 @@ public sealed partial class SpeedGraphPage : Page
     private void BtnNormal_Click(object sender, RoutedEventArgs e)
     {
         SpeedGraphSample.NormalGraph();
+    }
+
+    private async void BtnCopy_Click2(object sender, RoutedEventArgs e)
+    {
+        ulong _totalBytes = 1024UL * 1024 * (ulong)NBFileSize2.Value;
+
+        SpeedGraphSample2.SetGraphNormal();
+
+        if (_isSimulating2)
+            return;
+
+        _isSimulating2 = true;
+
+        ulong copiedBytes = 0;
+
+        while (copiedBytes < _totalBytes)
+        {
+            // Simulate varying copy speed (8–100 MB/s)
+            ulong speed = (ulong)(_random2.Next(8, 100) * 1024 * 1024);
+
+            copiedBytes += speed / 3;
+            if (copiedBytes > _totalBytes)
+                copiedBytes = _totalBytes;
+
+            double percent = (double)copiedBytes / _totalBytes * 100.0;
+
+            SpeedGraphSample2.Points.Add(new System.Numerics.Vector2((float)percent, speed));
+            await Task.Delay(100);
+        }
+
+        _isSimulating2 = false;
+    }
+
+    private void BtnError_Click2(object sender, RoutedEventArgs e)
+    {
+        SpeedGraphSample2.SetGraphError();
+    }
+
+    private void BtnPause_Click2(object sender, RoutedEventArgs e)
+    {
+        SpeedGraphSample2.SetGraphWarning();
+    }
+
+    private void BtnNormal_Click2(object sender, RoutedEventArgs e)
+    {
+        SpeedGraphSample2.SetGraphNormal();
+    }
+    private void BtnSuccess_Click2(object sender, RoutedEventArgs e)
+    {
+        SpeedGraphSample2.SetGraphSuccess();
     }
 }
