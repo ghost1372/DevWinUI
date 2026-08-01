6e12ccc2af78660fcce6cd184da8021255b7c9ef
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-08-01 11:23:44 +0330

    Add SecondHand

--- dev/DevWinUI/Controls/Native/Date/RealClock/RealClock.Property.cs
+++ dev/DevWinUI/Controls/Native/Date/RealClock/RealClock.Property.cs
@@ -1,21 +1,22 @@
-namespace DevWinUI;
+﻿namespace DevWinUI;
 public partial class RealClock
 {
-    public static readonly DependencyProperty ClockCornerRadiusProperty =
-        DependencyProperty.Register(nameof(ClockCornerRadius), typeof(CornerRadius), typeof(RealClock), new PropertyMetadata(new CornerRadius(0)));
     public CornerRadius ClockCornerRadius
     {
         get { return (CornerRadius)GetValue(ClockCornerRadiusProperty); }
         set { SetValue(ClockCornerRadiusProperty, value); }
     }
+    public static readonly DependencyProperty ClockCornerRadiusProperty =
+        DependencyProperty.Register(nameof(ClockCornerRadius), typeof(CornerRadius), typeof(RealClock), new PropertyMetadata(new CornerRadius(0)));
 
-    public static readonly DependencyProperty TimeZoneIdProperty = DependencyProperty.Register(
-        nameof(TimeZoneId), typeof(string), typeof(RealClock), new PropertyMetadata(null, OnTimeZoneIdChanged));
     public string TimeZoneId
     {
         get => (string)GetValue(TimeZoneIdProperty);
         set => SetValue(TimeZoneIdProperty, value);
     }
+    public static readonly DependencyProperty TimeZoneIdProperty = DependencyProperty.Register(
+        nameof(TimeZoneId), typeof(string), typeof(RealClock), new PropertyMetadata(null, OnTimeZoneIdChanged));
+    
     private static void OnTimeZoneIdChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         var ctl = (RealClock)d;
@@ -25,14 +26,13 @@ public partial class RealClock
         }
     }
 
-    public static readonly DependencyProperty TimeFormatProperty = DependencyProperty.Register(
-        nameof(TimeFormat), typeof(string), typeof(RealClock), new PropertyMetadata("HH:mm", OnTimeFormatChanged));
-
     public string TimeFormat
     {
         get => (string)GetValue(TimeFormatProperty);
         set => SetValue(TimeFormatProperty, value);
     }
+    public static readonly DependencyProperty TimeFormatProperty = DependencyProperty.Register(
+        nameof(TimeFormat), typeof(string), typeof(RealClock), new PropertyMetadata("HH:mm", OnTimeFormatChanged));
     private static void OnTimeFormatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         var ctl = (RealClock)d;
@@ -42,111 +42,117 @@ public partial class RealClock
         }
     }
 
-    public static readonly DependencyProperty CenterPointFillProperty =
-        DependencyProperty.Register(nameof(CenterPointFill), typeof(SolidColorBrush), typeof(RealClock), new PropertyMetadata(default(SolidColorBrush)));
-    public SolidColorBrush CenterPointFill
+    public Brush CenterPointFill
     {
-        get { return (SolidColorBrush)GetValue(CenterPointFillProperty); }
+        get { return (Brush)GetValue(CenterPointFillProperty); }
         set { SetValue(CenterPointFillProperty, value); }
     }
+    public static readonly DependencyProperty CenterPointFillProperty =
+        DependencyProperty.Register(nameof(CenterPointFill), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
 
-
-    public static readonly DependencyProperty CenterPointStrokeProperty =
-        DependencyProperty.Register(nameof(CenterPointStroke), typeof(SolidColorBrush), typeof(RealClock), new PropertyMetadata(default(SolidColorBrush)));
-    public SolidColorBrush CenterPointStroke
+    public Brush CenterPointStroke
     {
-        get { return (SolidColorBrush)GetValue(CenterPointStrokeProperty); }
+        get { return (Brush)GetValue(CenterPointStrokeProperty); }
         set { SetValue(CenterPointStrokeProperty, value); }
     }
+    public static readonly DependencyProperty CenterPointStrokeProperty =
+        DependencyProperty.Register(nameof(CenterPointStroke), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
 
-    public static readonly DependencyProperty CenterPointHeightProperty =
-        DependencyProperty.Register(nameof(CenterPointHeight), typeof(double), typeof(RealClock), new PropertyMetadata(8.0));
     public double CenterPointHeight
     {
         get { return (double)GetValue(CenterPointHeightProperty); }
         set { SetValue(CenterPointHeightProperty, value); }
     }
+    public static readonly DependencyProperty CenterPointHeightProperty =
+        DependencyProperty.Register(nameof(CenterPointHeight), typeof(double), typeof(RealClock), new PropertyMetadata(8.0));
 
-    public static readonly DependencyProperty CenterPointWidthProperty =
-        DependencyProperty.Register(nameof(CenterPointWidth), typeof(double), typeof(RealClock), new PropertyMetadata(8.0));
     public double CenterPointWidth
     {
         get { return (double)GetValue(CenterPointWidthProperty); }
         set { SetValue(CenterPointWidthProperty, value); }
     }
+    public static readonly DependencyProperty CenterPointWidthProperty =
+        DependencyProperty.Register(nameof(CenterPointWidth), typeof(double), typeof(RealClock), new PropertyMetadata(8.0));
 
-    public static readonly DependencyProperty MinuteHandBackgroundProperty =
-        DependencyProperty.Register(nameof(MinuteHandBackground), typeof(SolidColorBrush), typeof(RealClock), new PropertyMetadata(default(SolidColorBrush)));
-    public SolidColorBrush MinuteHandBackground
+    public Brush SecondHandBackground
     {
-        get { return (SolidColorBrush)GetValue(MinuteHandBackgroundProperty); }
+        get { return (Brush)GetValue(SecondHandBackgroundProperty); }
+        set { SetValue(SecondHandBackgroundProperty, value); }
+    }
+
+    public static readonly DependencyProperty SecondHandBackgroundProperty =
+        DependencyProperty.Register(nameof(SecondHandBackground), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
+
+    public Brush MinuteHandBackground
+    {
+        get { return (Brush)GetValue(MinuteHandBackgroundProperty); }
         set { SetValue(MinuteHandBackgroundProperty, value); }
     }
+    public static readonly DependencyProperty MinuteHandBackgroundProperty =
+        DependencyProperty.Register(nameof(MinuteHandBackground), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
 
-    public static readonly DependencyProperty TitleBorderBackgroundProperty =
-        DependencyProperty.Register(nameof(TitleBorderBackground), typeof(SolidColorBrush), typeof(RealClock), new PropertyMetadata(default(SolidColorBrush)));
-    public SolidColorBrush TitleBorderBackground
+    public Brush TitleBorderBackground
     {
-        get { return (SolidColorBrush)GetValue(TitleBorderBackgroundProperty); }
+        get { return (Brush)GetValue(TitleBorderBackgroundProperty); }
         set { SetValue(TitleBorderBackgroundProperty, value); }
     }
+    public static readonly DependencyProperty TitleBorderBackgroundProperty =
+        DependencyProperty.Register(nameof(TitleBorderBackground), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
 
-    public static readonly DependencyProperty TitleBorderCornerRadiusProperty =
-        DependencyProperty.Register(nameof(TitleBorderCornerRadius), typeof(CornerRadius), typeof(RealClock), new PropertyMetadata(default(CornerRadius)));
     public CornerRadius TitleBorderCornerRadius
     {
         get { return (CornerRadius)GetValue(TitleBorderCornerRadiusProperty); }
         set { SetValue(TitleBorderCornerRadiusProperty, value); }
     }
+    public static readonly DependencyProperty TitleBorderCornerRadiusProperty =
+        DependencyProperty.Register(nameof(TitleBorderCornerRadius), typeof(CornerRadius), typeof(RealClock), new PropertyMetadata(default(CornerRadius)));
 
-
-    public static readonly DependencyProperty ClockWidthProperty =
-        DependencyProperty.Register(nameof(ClockWidth), typeof(double), typeof(RealClock), new PropertyMetadata(250.0));
     public double ClockWidth
     {
         get { return (double)GetValue(ClockWidthProperty); }
         set { SetValue(ClockWidthProperty, value); }
     }
+    public static readonly DependencyProperty ClockWidthProperty =
+        DependencyProperty.Register(nameof(ClockWidth), typeof(double), typeof(RealClock), new PropertyMetadata(250.0));
 
-
-    public static readonly DependencyProperty ClockHeightProperty =
-        DependencyProperty.Register(nameof(ClockHeight), typeof(double), typeof(RealClock), new PropertyMetadata(250.0));
     public double ClockHeight
     {
         get { return (double)GetValue(ClockHeightProperty); }
         set { SetValue(ClockHeightProperty, value); }
     }
+    public static readonly DependencyProperty ClockHeightProperty =
+        DependencyProperty.Register(nameof(ClockHeight), typeof(double), typeof(RealClock), new PropertyMetadata(250.0));
 
-    public static readonly DependencyProperty ClockBackgroundProperty =
-        DependencyProperty.Register(nameof(ClockBackground), typeof(SolidColorBrush), typeof(RealClock), new PropertyMetadata(default(SolidColorBrush)));
-    public SolidColorBrush ClockBackground
+    public Brush ClockBackground
     {
-        get { return (SolidColorBrush)GetValue(ClockBackgroundProperty); }
+        get { return (Brush)GetValue(ClockBackgroundProperty); }
         set { SetValue(ClockBackgroundProperty, value); }
     }
+    public static readonly DependencyProperty ClockBackgroundProperty =
+        DependencyProperty.Register(nameof(ClockBackground), typeof(Brush), typeof(RealClock), new PropertyMetadata(default(Brush)));
 
-    public static readonly DependencyProperty OffsetAngleProperty = DependencyProperty.Register(
-        nameof(OffsetAngle), typeof(double), typeof(RealClock), new PropertyMetadata(0.0));
     public double OffsetAngle
     {
         get => (double)GetValue(OffsetAngleProperty);
         set => SetValue(OffsetAngleProperty, value);
     }
+    public static readonly DependencyProperty OffsetAngleProperty = DependencyProperty.Register(
+        nameof(OffsetAngle), typeof(double), typeof(RealClock), new PropertyMetadata(0.0));
 
-
-    public static readonly DependencyProperty DiameterProperty =
-        DependencyProperty.Register(nameof(Diameter), typeof(double), typeof(RealClock), new PropertyMetadata(170.0));
     public double Diameter
     {
         get => (double)GetValue(DiameterProperty);
         set => SetValue(DiameterProperty, value);
     }
+    public static readonly DependencyProperty DiameterProperty =
+        DependencyProperty.Register(nameof(Diameter), typeof(double), typeof(RealClock), new PropertyMetadata(170.0));
 
-    public static readonly DependencyProperty HeaderMarginProperty =
-        DependencyProperty.Register(nameof(HeaderMargin), typeof(Thickness), typeof(RealClock), new PropertyMetadata(new Thickness(4)));
     public Thickness HeaderMargin
     {
         get { return (Thickness)GetValue(HeaderMarginProperty); }
         set { SetValue(HeaderMarginProperty, value); }
     }
+
+    public static readonly DependencyProperty HeaderMarginProperty =
+        DependencyProperty.Register(nameof(HeaderMargin), typeof(Thickness), typeof(RealClock), new PropertyMetadata(new Thickness(4)));
 }
--- dev/DevWinUI/Controls/Native/Date/RealClock/RealClock.cs
+++ dev/DevWinUI/Controls/Native/Date/RealClock/RealClock.cs
@@ -1,9 +1,10 @@
-using Microsoft.UI.Xaml.Shapes;
+﻿using Microsoft.UI.Xaml.Shapes;
 
 namespace DevWinUI;
 [TemplatePart(Name = ElementButtonAm, Type = typeof(RadioButton))]
 [TemplatePart(Name = ElementButtonPm, Type = typeof(RadioButton))]
 [TemplatePart(Name = ElementBorderTitle, Type = typeof(Border))]
+[TemplatePart(Name = ElementSecondHandLine, Type = typeof(Line))]
 [TemplatePart(Name = ElementMinuteHandLine, Type = typeof(Line))]
 [TemplatePart(Name = ElementPanelNum, Type = typeof(CirclePanel))]
 [TemplatePart(Name = ElementTimeStr, Type = typeof(TextBlock))]
@@ -13,6 +14,7 @@ public partial class RealClock : Control
     private const string ElementButtonPm = "PART_ButtonPm";
     private const string ElementBorderTitle = "PART_BorderTitle";
     private const string ElementMinuteHandLine = "PART_MinuteHand";
+    private const string ElementSecondHandLine = "PART_SecondHand";
     private const string ElementPanelNum = "PART_PanelNum";
     private const string ElementTimeStr = "PART_TimeStr";
 
@@ -20,8 +22,10 @@ public partial class RealClock : Control
 
     private ClockRadioButton _buttonPm;
 
+    private Line _secondHandLine;
     private Line _minuteHandLine;
 
+    private RotateTransform _secondHandRotateTransform;
     private RotateTransform _rotateTransformClock;
 
     private CirclePanel _circlePanel;
@@ -49,6 +53,7 @@ public partial class RealClock : Control
 
         _buttonAm = GetTemplateChild(ElementButtonAm) as ClockRadioButton;
         _buttonPm = GetTemplateChild(ElementButtonPm) as ClockRadioButton;
+        _secondHandLine = GetTemplateChild(ElementSecondHandLine) as Line;
         _minuteHandLine = GetTemplateChild(ElementMinuteHandLine) as Line;
         _circlePanel = GetTemplateChild(ElementPanelNum) as CirclePanel;
         _blockTime = GetTemplateChild(ElementTimeStr) as TextBlock;
@@ -57,6 +62,9 @@ public partial class RealClock : Control
 
         _rotateTransformClock = new RotateTransform();
         _minuteHandLine.RenderTransform = _rotateTransformClock;
+
+        _secondHandRotateTransform = new RotateTransform();
+        _secondHandLine.RenderTransform = _secondHandRotateTransform;
 
         _hourButtonList = new List<ClockRadioButton>();
         _circlePanel.Children.Clear();
@@ -81,12 +89,17 @@ public partial class RealClock : Control
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
         Update();
+
         if (_timer == null)
         {
-            _timer = new DispatcherTimer();
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+
             _timer.Tick += OnTimerTick;
         }
-        _timer.Interval = GetIntervalToNextMinute();
+
         _timer.Start();
     }
 
@@ -98,18 +111,12 @@ public partial class RealClock : Control
     private void OnTimerTick(object sender, object e)
     {
         Update();
-        _timer.Interval = GetIntervalToNextMinute();
-    }
-
-    private static TimeSpan GetIntervalToNextMinute()
-    {
-        return TimeSpan.FromSeconds(61 - DateTime.Now.Second);
     }
 
     private bool CheckNull()
     {
         if (_buttonPm == null || _buttonAm == null ||
-            _minuteHandLine == null || _circlePanel == null ||
+            _secondHandLine == null || _minuteHandLine == null || _circlePanel == null ||
             _blockTime == null) return false;
 
         return true;
@@ -132,15 +139,17 @@ public partial class RealClock : Control
             _buttonAm.IsChecked = true;
         }
 
-        _rotateTransformClock.Angle = time.Minute * 6;
+        _secondHandRotateTransform.Angle = (float)(time.Second * 6 + time.Millisecond * 0.006);
+
+        _rotateTransformClock.Angle = (float)(time.Minute * 6 + time.Second * 0.1);
 
         var hour12 = time.Hour % 12;
         if (hour12 == 0) hour12 = 12;
+
         _hourButtonList[hour12 - 1].IsChecked = true;
 
         _blockTime.Text = time.ToString(TimeFormat);
     }
-
     private TimeZoneInfo ResolveTimeZone()
     {
         if (string.IsNullOrEmpty(TimeZoneId)) return TimeZoneInfo.Local;
